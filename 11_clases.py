56da204a759fe19c6bd61c4f1a39a3072eb8e742
Diego Pasten <diegopasten78@gmail.com>
2025-08-05 23:28:33 -0400

    feat: :package: Clases

--- 11_clases.py
+++ 11_clases.py
@@ -3,6 +3,7 @@
 class EmptyPerson:
     pass
 
+
 print(EmptyPerson)
 
 
@@ -22,7 +23,7 @@ print(my_person.defecto)
 # Otro ejemplo
 class MyPerson:
     def __init__(self, name, surname):
-        self.full_name = f"{name} {surname}"
+        self.full_name = f"{name} {surname} 🧔"
 
     # aquí deberi pasarle "self" para acceder a usus propiedades
     def walk(self):
@@ -32,3 +33,24 @@ class MyPerson:
 person = MyPerson("Christopher", "Smith")
 print(person.full_name)
 person.walk()
+
+print("--------------------------------------")
+
+# Datos privados
+class Cat:
+    def __init__(self, name, age, color, owner):
+        self.cat_date = f"{name}, tiene {age} años y es de color {color} 🐈"
+        self.__owner = owner # Propiedad privada
+
+    def get_name(self):
+        return self.__owner # Método público para acceder al atributo privado
+
+my_cat = Cat("Dalpo", 3, "Gris con manchas blancas", "Diego")
+print(my_cat.cat_date)
+print(my_cat.get_name())
+
+# print(my_cat.__owner) <-- Error: AttributeError
+# En este caso pasamos el atributo privado a un método 
+# public para poder acceder a el
+
+        
